8839c61144b0ba50ee78fd899aadee6f112a6474
Kane McGregor <kanemcgregor@Kanes-MacBook-Air-2.local>
2026-03-26 18:25:08 +1300

    fix: correct speech_model field in AssemblyAI requests

--- server.js
+++ server.js
@@ -542,7 +542,7 @@ app.post('/translate', upload.single('video'), async (req, res) => {
 
           const transcriptRes = await axios.post('https://api.assemblyai.com/v2/transcript', {
             audio_url: uploadRes.data.upload_url,
-            speech_models: ['universal-2'],
+            speech_model: 'universal-2',
           }, {
             headers: { 'authorization': process.env.ASSEMBLYAI_API_KEY },
             timeout: 30000
